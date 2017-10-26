f372da61e2c6ceedddccd48557a54c7537b3af02
Viktor <vik24rus@gmail.com>
2017-10-27 01:08:35 +0700

    add humidity number1

--- Termometr/Termometr.ino
+++ Termometr/Termometr.ino
@@ -1,21 +1,18 @@
-//We always have to include the library
 #include "LedControlMS.h"
 #include "OneWire.h"
-
-
 /*
- Now we need a LedControl to work with.
- ***** These pin numbers will probably not work with your hardware *****
  pin 12 is connected to the DataIn 
  pin 11 is connected to the CLK 
- pin 6 is connected to CS
- We have only a single MAX72XX.
- */
-LedControl lc=LedControl(12,11,10,2);
-OneWire  ds(9);  // on pin 10 (a 4.7K resistor is necessary)
+ pin 10 is connected to CS
+ pin 9 DS18B20 with 4.7K resistor
 
-byte out[8]=    {B00111100,B01000010,B10010101,B10010001,B10010001, B10010101, B01000010, B00111100};
-byte numbers[100][8]= {
+ while get temp from sensors , we get average 4 times
+ */
+LedControl lc=LedControl(12,11,10,2); // 2 - two 8x8 led matrix
+OneWire  ds(9);  
+
+byte out[8]=    {B00111100,B01000010,B10010101,B10010001,B10010001, B10010101, B01000010, B00111100}; // smile on LED 1, while we get temp from street
+byte numbersT[100][8]= {
   {B00000000, B00000000, B00000000, B00000000, B00000000, B11111111, B10000001, B11111111},//0
   {B00000000, B00000000, B00000000, B00000000, B00000000, B00000100, B00000010, B11111111},//1
   {B00000000, B00000000, B00000000, B00000000, B00000000, B11110011, B10001001, B10000111},//2
@@ -121,90 +118,18 @@ byte numbers[100][8]= {
 
 
 void setup() {
-  /*
-   The MAX72XX is in power-saving mode on startup,
-   we have to do a wakeup call
-   */
+
   Serial.begin(9600);
   lc.shutdown(0,false);
-  /* Set the brightness to a medium values */
   lc.setIntensity(0,2);
-  /* and clear the display */
   lc.clearDisplay(0);
-  
   lc.shutdown(1,false);
-  /* Set the brightness to a medium values */
   lc.setIntensity(1,8);
-  /* and clear the display */
   lc.clearDisplay(1);
   
 }
 
-/*
- This method will display the characters for the
- word "Arduino" one after the other on the matrix. 
- (you need at least 5x7 leds to see the whole chars)
- */
-//void writeArduinoOnMatrix() {
-//  /* here is the data for the characters */
-//  byte a[5]={B01111110,B10001000,B10001000,B10001000,B01111110};
-//  byte r[5]={B00111110,B00010000,B00100000,B00100000,B00010000};
-//  byte d[5]={B00011100,B00100010,B00100010,B00010010,B11111110};
-//  byte u[5]={B00111100,B00000010,B00000010,B00000100,B00111110};
-//  byte i[5]={B00000000,B00100010,B10111110,B00000010,B00000000};
-//  byte n[5]={B00111110,B00010000,B00100000,B00100000,B00011110};
-//  byte o[5]={B00011100,B00100010,B00100010,B00100010,B00011100};
-//
-//  /* now display them one by one with a small delay */
-//  lc.setRow(0,0,a[0]);
-//  lc.setRow(0,1,a[1]);
-//  lc.setRow(0,2,a[2]);
-//  lc.setRow(0,3,a[3]);
-//  lc.setRow(0,4,a[4]);
-//  delay(delaytime);
-//  lc.setRow(0,0,r[0]);
-//  lc.setRow(0,1,r[1]);
-//  lc.setRow(0,2,r[2]);
-//  lc.setRow(0,3,r[3]);
-//  lc.setRow(0,4,r[4]);
-//  delay(delaytime);
-//  lc.setRow(0,0,d[0]);
-//  lc.setRow(0,1,d[1]);
-//  lc.setRow(0,2,d[2]);
-//  lc.setRow(0,3,d[3]);
-//  lc.setRow(0,4,d[4]);
-//  delay(delaytime);
-//  lc.setRow(0,0,u[0]);
-//  lc.setRow(0,1,u[1]);
-//  lc.setRow(0,2,u[2]);
-//  lc.setRow(0,3,u[3]);
-//  lc.setRow(0,4,u[4]);
-//  delay(delaytime);
-//  lc.setRow(0,0,i[0]);
-//  lc.setRow(0,1,i[1]);
-//  lc.setRow(0,2,i[2]);
-//  lc.setRow(0,3,i[3]);
-//  lc.setRow(0,4,i[4]);
-//  delay(delaytime);
-//  lc.setRow(0,0,n[0]);
-//  lc.setRow(0,1,n[1]);
-//  lc.setRow(0,2,n[2]);
-//  lc.setRow(0,3,n[3]);
-//  lc.setRow(0,4,n[4]);
-//  delay(delaytime);
-//  lc.setRow(0,0,o[0]);
-//  lc.setRow(0,1,o[1]);
-//  lc.setRow(0,2,o[2]);
-//  lc.setRow(0,3,o[3]);
-//  lc.setRow(0,4,o[4]);
-//  delay(delaytime);
-//  lc.setRow(0,0,0);
-//  lc.setRow(0,1,0);
-//  lc.setRow(0,2,0);
-//  lc.setRow(0,3,0);
-//  lc.setRow(0,4,0);
-//  delay(delaytime);
-//}
+
 
 /*
   This function lights up a some Leds in a row.
@@ -368,95 +293,29 @@ void setup() {
 //  
 //}
 
-//void draw_err(){
-//  byte err[8]={B11100000,B11100000,B11100000,B11111111,B11111111, B11100000, B11100000, B11100000};
-//  for (int i=0;  i<8 ; i++){
-//    lc.setRow(0,i,err[i]);
-//  }
-//}
-//
-//void worker(){
-//  draw(2, true, 0);
-//  delay(1000);
-//}
+void draw(int temp_room, bool positive, int number_room){ 
+  if (number_room == 0){ 
+   
+  }
 
-//void draw(int temp_room, bool positive, int number_room){ 
-//  if (number_room == 0){
-//    lc.setLed(0,0,7,true);
-//    if(positive == true){29
-//      switch ( temp_room ) {
-//        case 1:         
-//          break;
-// 
-//        case 2:
-//        
-//        for (int i=0;  i<8 ; i++){
-//          lc.setRow(0,i,number2[i]);
-//        }
-//         delay(2000);        
-// void draw_err(){
-//  byte err[8]={B11100000,B11100000,B11100000,B11111111,B11111111, B11100000, B11100000, B11100000};
-//  for (int i=0;  i<8 ; i++){
-//    lc.setRow(0,i,err[i]);
-//  }
-//}
+  if (number_room == 1){    
+   
+  }
 
-
-//void draw(int temp_room, bool positive, int number_room){ 
-//  if (number_room == 0){
-//    lc.setLed(0,0,7,true);
-//    if(positive == true){
-//      switch ( temp_room ) {
-//        case 1:         
-//          break;
-// 
-//        case 2:
-//        
-//        for (int i=0;  i<8 ; i++){
-//          lc.setRow(0,i,number2[i]);
-//        }
-//         delay(2000);        
-//          break;
-//        
-//        default:          
-//          break;
-//        }
-//    }else{
-//      lc.setLed(0,0,4,true);
-//    }
-//  }
-//
-//  if (number_room == 1){
-//    lc.setLed(0,0,7,true);
-//    lc.setLed(0,0,6,true);
-//    if(positive == true){
-//      
-//    }else{
-//      lc.setLed(0,0,4,true);
-//    }
-//  }         break;
-//        
-//        default:          
-//          break;
-//        }
-//    }else{
-//      lc.setLed(0,0,4,true);
-//    }
-//  }
-//
-//  if (number_room == 1){
-//    lc.setLed(0,0,7,true);
-//    lc.setLed(0,0,6,true);
-//    if(positive == true){
-//      
-//    }else{
-//      lc.setLed(0,0,4,true);
-//    }
-//  }
+  if (number_room == 2){ //street
+   
+  }
   
-//}
-void loop() {
+}
 
+void loop() {
+//humidity
+byte numberH[5]=    {B00000001,B00000001,B00000001,B00000011,B00000001}; //1
+byte plus_out[8]=    {B00000010,B00000111,B00000010,B00000000,B00000000, B00000000, B00000000, B00000000};
+  
+for (int j=0;  j<5 ; j++){
+      lc.setColumn(0,j,numberH[j]);
+}
 //  for (int i=0; i<100 ; i++){ 
 //    delay(600);
 //    lc.clearDisplay(0);
